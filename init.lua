a94aa7e7514dcca52d9263fc1cb2df82c7ea92fd
markanesko <markoberic94@gmail.com>
2025-03-25 20:27:17 +0100

    added toggle term with initial configuration

--- init.lua
+++ init.lua
@@ -987,7 +987,7 @@ require('lazy').setup({
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.lazy-git',
   require 'kickstart.plugins.blame',
-
+  require 'kickstart.plugins.toggle-term',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
---
+++ lua/kickstart/plugins/toggle-term.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = true,
+    keys = {
+      { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = '[T]oggle [T]erm' },
+    },
+  },
+}
